83c9078e3d9ca56ae6d4f126f9d71681961a2f3f
migurski <migurski@ubuntu.(none)>
2010-09-04 12:23:44 -0700

    Putting the map image on a PDF

--- compose.py
+++ compose.py
@@ -1,10 +1,13 @@
 """ Generate a PDF of a meeting point map.
 """
 
+from os import close, unlink
+from tempfile import mkstemp
 from optparse import OptionParser
 from ModestMaps import mapByExtentZoom
 from ModestMaps.Geo import Location
 from ModestMaps.OpenStreetMap import Provider
+from cairo import PDFSurface, ImageSurface, Context
 
 parser = OptionParser()
 
@@ -23,4 +26,19 @@ if __name__ == '__main__':
     lat1, lon1, lat2, lon2 = options.bbox
     mmap = mapByExtentZoom(osm, Location(lat1, lon1), Location(lat2, lon2), 16)
     
-    mmap.draw(True).save('out.png')
+    handle, filename = mkstemp(suffix='.png')
+    close(handle)
+    
+    mmap.draw(True).save(filename)
+    
+    surf = PDFSurface('out.pdf', 612, 792)
+    
+    img = ImageSurface.create_from_png(filename)
+    
+    ctx = Context(surf)
+    
+    ctx.set_source_surface(img, 10, 10)
+    ctx.paint()
+    
+    surf.finish()
+    unlink(filename)
