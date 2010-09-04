916dbfdbb325bb0907101de4058c5aae62ef7567
migurski <migurski@ubuntu.(none)>
2010-09-04 13:15:05 -0700

    Added mapByExtentZoomAspect()

--- compose.py
+++ compose.py
@@ -4,8 +4,9 @@
 from os import close, unlink
 from tempfile import mkstemp
 from optparse import OptionParser
-from ModestMaps import mapByExtentZoom
+from ModestMaps import mapByExtentZoom, mapByCenterZoom
 from ModestMaps.Geo import Location
+from ModestMaps.Core import Point
 from ModestMaps.Providers import TemplatedMercatorProvider
 from cairo import PDFSurface, ImageSurface, Context
 
@@ -14,6 +15,22 @@ inppt = 0.013888889
 
 ptpin = 1./inppt
 ptpmm = 1./mmppt
+
+def mapByExtentZoomAspect(prov, locA, locB, zoom, aspect):
+    """
+    """
+    mmap = mapByExtentZoom(prov, Location(lat1, lon1), Location(lat2, lon2), zoom)
+    center = mmap.pointLocation(Point(mmap.dimensions.x/2, mmap.dimensions.y/2))
+    
+    if aspect < float(mmap.dimensions.x) / float(mmap.dimensions.y):
+        # make taller
+        dimensions = Point(mmap.dimensions.x, int(mmap.dimensions.x / aspect))
+
+    else:
+        # make wider
+        dimensions = Point(int(mmap.dimensions.y * aspect), mmap.dimensions.y)
+    
+    return mapByCenterZoom(prov, center, zoom, dimensions)
 
 parser = OptionParser()
 
@@ -28,11 +45,9 @@ parser.add_option('-b', '--bbox', dest='bbox',
 if __name__ == '__main__':
     options, args = parser.parse_args()
     
-    prov = TemplatedMercatorProvider('http://localhost/~migurski/TileStache/tilestache.cgi/osm/{Z}/{X}/{Y}.png')
+    prov = TemplatedMercatorProvider('http://127.0.0.1/~migurski/TileStache/tilestache.cgi/osm/{Z}/{X}/{Y}.png')
     lat1, lon1, lat2, lon2 = options.bbox
-    mmap = mapByExtentZoom(prov, Location(lat1, lon1), Location(lat2, lon2), 16)
-    
-    print mmap.dimensions
+    mmap = mapByExtentZoomAspect(prov, Location(lat1, lon1), Location(lat2, lon2), 16, 1)
     
     handle, filename = mkstemp(suffix='.png')
     close(handle)
@@ -51,14 +66,8 @@ if __name__ == '__main__':
 
     ctx.save()
     ctx.scale(mmppt, mmppt)
-    ctx.set_source_surface(img, 0, 0)
-    ctx.paint()
-    ctx.restore()
-
-    ctx.translate(10, 10)
-
-    ctx.save()
-    ctx.scale(mmppt, mmppt)
+    scale = 195.9 * ptpmm / img.get_width()
+    ctx.scale(scale, scale)
     ctx.set_source_surface(img, 0, 0)
     ctx.paint()
     ctx.restore()
