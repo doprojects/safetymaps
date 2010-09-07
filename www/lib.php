7c5adbfdfae41218ed6c37411c95c9694b3c4b33
migurski <migurski@ubuntu.(none)>
2010-09-06 18:33:55 -0700

    Moved a bunch of functions off to new file lib.php

---
+++ www/lib.php
@@ -0,0 +1,216 @@
+<?php
+
+   /**
+    * id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    * 
+    * name    TINYTEXT,
+    * email   TINYTEXT
+    */
+    function getset_user($db, $args)
+    {
+        $_name = mysql_real_escape_string($args['name'], $db);
+        $_email = mysql_real_escape_string($args['email'], $db);
+    
+        $q = "SELECT id FROM users
+              WHERE name  = '{$_name}'
+                AND email = '{$_email}'";
+
+        if($res = mysql_query($q, $db))
+        {
+            $row = mysql_fetch_assoc($res);
+            
+            if($row) {
+                return $row['id'];
+            
+            } else {
+                $q = "INSERT INTO users
+                      SET name  = '{$_name}',
+                          email = '{$_email}'";
+
+                if($res = mysql_query($q, $db))
+                    return mysql_insert_id($db);
+            }
+        }
+        
+        return null;
+    }
+    
+   /**
+    * id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    * user_id     INT UNSIGNED NOT NULL,
+    * 
+    * place_name  TINYTEXT,
+    * place_lat   DOUBLE,
+    * place_lon   DOUBLE,
+    * 
+    * emergency   TINYTEXT,
+    * note_full   TEXT,
+    * note_short  TEXT,
+    * 
+    * paper       ENUM('a4', 'letter') DEFAULT 'letter',
+    * format      ENUM('4up', '2up-fridge', 'poster') DEFAULT '2up-fridge',
+    * 
+    * bbox_north  DOUBLE,
+    * bbox_south  DOUBLE,
+    * bbox_east   DOUBLE,
+    * bbox_west   DOUBLE,
+    * 
+    * created     DATETIME,
+    * privacy     ENUM('public', 'unlisted') DEFAULT 'public',
+    */
+    function set_map($db, $args)
+    {
+        $_user_id = sprintf('%d', $args['user_id']);
+        
+        $_place_name = mysql_real_escape_string($args['place_name'], $db);
+        $_place_lat = sprintf('%.6f', $args['place_lat']);
+        $_place_lon = sprintf('%.6f', $args['place_lon']);
+
+        $_emergency = mysql_real_escape_string($args['emergency'], $db);
+        $_note_full = mysql_real_escape_string($args['note_full'], $db);
+        $_note_short = mysql_real_escape_string($args['note_short'], $db);
+
+        $_paper = mysql_real_escape_string($args['paper'], $db);
+        $_format = mysql_real_escape_string($args['format'], $db);
+
+        $_bbox_north = sprintf('%.6f', $args['bbox_north']);
+        $_bbox_south = sprintf('%.6f', $args['bbox_south']);
+        $_bbox_east = sprintf('%.6f', $args['bbox_east']);
+        $_bbox_west = sprintf('%.6f', $args['bbox_west']);
+
+        $_privacy = mysql_real_escape_string($args['privacy'], $db);
+        
+        $q = "INSERT INTO maps
+              SET user_id    = {$_user_id},
+                  place_name = '{$_place_name}',
+                  place_lat  = {$_place_lat},
+                  place_lon  = {$_place_lon},
+                  emergency  = '{$_emergency}',
+                  note_full  = '{$_note_full}',
+                  note_short = '{$_note_short}',
+                  paper      = '{$_paper}',
+                  format     = '{$_format}',
+                  bbox_north = {$_bbox_north},
+                  bbox_south = {$_bbox_south},
+                  bbox_east  = {$_bbox_east},
+                  bbox_west  = {$_bbox_west},
+                  created    = NOW(),
+                  privacy    = '{$_privacy}'";
+
+        if($res = mysql_query($q, $db))
+            return mysql_insert_id($db);
+
+        return null;
+    }
+
+   /**
+    * id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    * user_id INT UNSIGNED NOT NULL,
+    * map_id  INT UNSIGNED NOT NULL,
+    * 
+    * name    TINYTEXT,
+    * email   TINYTEXT,
+    * 
+    * sent    DATETIME,
+    */
+    function set_recipient($db, $args)
+    {
+        $_user_id = sprintf('%d', $args['user_id']);
+        $_map_id = sprintf('%d', $args['map_id']);
+    
+        $_name = mysql_real_escape_string($args['name'], $db);
+        $_email = mysql_real_escape_string($args['email'], $db);
+    
+        $q = "INSERT INTO recipients
+              SET user_id = {$_user_id},
+                  map_id  = {$_map_id},
+                  name    = '{$_name}',
+                  email   = '{$_email}',
+                  sent    = NULL";
+
+        if($res = mysql_query($q, $db))
+            return mysql_insert_id($db);
+        
+        return null;
+    }
+    
+   /**
+    * {
+    *   sender: { name: ___, email: ___ }
+    *   place:
+    *   {
+    *     name: ___,
+    *     location: [ ___, ___ ],
+    *     emergency: ___,
+    *     full-note: ___,
+    *     short-note: ___
+    *   },
+    *   map:
+    *   {
+    *     privacy: ___,
+    *     bounds: [ ___, ___, ___, ___ ],
+    *     paper: ___,
+    *     format: ___
+    *   },
+    *   recipients:
+    *   [
+    *     { name: ___, email: ___ },
+    *     { name: ___, email: ___ },
+    *     ...
+    *   ]
+    * }
+    */
+    function add_map($db, $args)
+    {
+        $commit_ok = false;
+        mysql_query('BEGIN', $db);
+
+        $user_id = getset_user($db, $args['sender']);
+        
+        if($user_id)
+        {
+            $map_args = array(
+                'user_id' => $user_id,
+    
+                'place_name' => $args['place']['name'],
+                'place_lat' => $args['place']['location'][0],
+                'place_lon' => $args['place']['location'][1],
+    
+                'emergency' => $args['place']['emergency'],
+                'note_full' => $args['place']['full-note'],
+                'note_short' => $args['place']['short-note'],
+    
+                'paper' => $args['map']['paper'],
+                'format' => $args['map']['format'],
+    
+                'bbox_north' => $args['map']['bounds'][0],
+                'bbox_south' => $args['map']['bounds'][1],
+                'bbox_east' => $args['map']['bounds'][2],
+                'bbox_west' => $args['map']['bounds'][3],
+    
+                'privacy' => $args['map']['privacy']
+            );
+            
+            $map_id = set_map($db, $map_args);
+            
+            if($map_id)
+            {
+                $commit_ok = true;
+            
+                foreach($args['recipients'] as $r => $recipient)
+                {
+                    $recipient['user_id'] = $user_id;
+                    $recipient['map_id'] = $map_id;
+                    $recipient_id = set_recipient($db, $recipient);
+                    
+                    if(!$recipient_id)
+                        $commit_ok = false;
+                }
+            }
+        }
+        
+        mysql_query($commit_ok ? 'COMMIT' : 'ROLLBACK');
+        return $commit_ok ? $map_id : null;
+    }
+
+?>
--- www/map.php
+++ www/map.php
@@ -1,192 +1,13 @@
 <?php
 
-   /**
-    * id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    * 
-    * name    TINYTEXT,
-    * email   TINYTEXT
-    */
-    function getset_user($db, $args)
-    {
-        $_name = mysql_real_escape_string($args['name'], $db);
-        $_email = mysql_real_escape_string($args['email'], $db);
-    
-        $q = "SELECT id FROM users
-              WHERE name  = '{$_name}'
-                AND email = '{$_email}'";
-
-        if($res = mysql_query($q, $db))
-        {
-            $row = mysql_fetch_assoc($res);
-            
-            if($row) {
-                return $row['id'];
-            
-            } else {
-                $q = "INSERT INTO users
-                      SET name  = '{$_name}',
-                          email = '{$_email}'";
-
-                if($res = mysql_query($q, $db))
-                    return mysql_insert_id($db);
-            }
-        }
-        
-        return null;
-    }
-    
-   /**
-    * id          INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    * user_id     INT UNSIGNED NOT NULL,
-    * 
-    * place_name  TINYTEXT,
-    * place_lat   DOUBLE,
-    * place_lon   DOUBLE,
-    * 
-    * emergency   TINYTEXT,
-    * note_full   TEXT,
-    * note_short  TEXT,
-    * 
-    * paper       ENUM('a4', 'letter') DEFAULT 'letter',
-    * format      ENUM('4up', '2up-fridge', 'poster') DEFAULT '2up-fridge',
-    * 
-    * bbox_north  DOUBLE,
-    * bbox_south  DOUBLE,
-    * bbox_east   DOUBLE,
-    * bbox_west   DOUBLE,
-    * 
-    * created     DATETIME,
-    * privacy     ENUM('public', 'unlisted') DEFAULT 'public',
-    */
-    function set_map($db, $args)
-    {
-        $_user_id = sprintf('%d', $args['user_id']);
-        
-        $_place_name = mysql_real_escape_string($args['place_name'], $db);
-        $_place_lat = sprintf('%.6f', $args['place_lat']);
-        $_place_lon = sprintf('%.6f', $args['place_lon']);
-
-        $_emergency = mysql_real_escape_string($args['emergency'], $db);
-        $_note_full = mysql_real_escape_string($args['note_full'], $db);
-        $_note_short = mysql_real_escape_string($args['note_short'], $db);
-
-        $_paper = mysql_real_escape_string($args['paper'], $db);
-        $_format = mysql_real_escape_string($args['format'], $db);
-
-        $_bbox_north = sprintf('%.6f', $args['bbox_north']);
-        $_bbox_south = sprintf('%.6f', $args['bbox_south']);
-        $_bbox_east = sprintf('%.6f', $args['bbox_east']);
-        $_bbox_west = sprintf('%.6f', $args['bbox_west']);
-
-        $_privacy = mysql_real_escape_string($args['privacy'], $db);
-        
-        $q = "INSERT INTO maps
-              SET user_id    = {$_user_id},
-                  place_name = '{$_place_name}',
-                  place_lat  = {$_place_lat},
-                  place_lon  = {$_place_lon},
-                  emergency  = '{$_emergency}',
-                  note_full  = '{$_note_full}',
-                  note_short = '{$_note_short}',
-                  paper      = '{$_paper}',
-                  format     = '{$_format}',
-                  bbox_north = {$_bbox_north},
-                  bbox_south = {$_bbox_south},
-                  bbox_east  = {$_bbox_east},
-                  bbox_west  = {$_bbox_west},
-                  created    = NOW(),
-                  privacy    = '{$_privacy}'";
-
-        if($res = mysql_query($q, $db))
-            return mysql_insert_id($db);
-
-        return null;
-    }
-
-   /**
-    * id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    * user_id INT UNSIGNED NOT NULL,
-    * map_id  INT UNSIGNED NOT NULL,
-    * 
-    * name    TINYTEXT,
-    * email   TINYTEXT,
-    * 
-    * sent    DATETIME,
-    */
-    function set_recipient($db, $args)
-    {
-        $_user_id = sprintf('%d', $args['user_id']);
-        $_map_id = sprintf('%d', $args['map_id']);
-    
-        $_name = mysql_real_escape_string($args['name'], $db);
-        $_email = mysql_real_escape_string($args['email'], $db);
-    
-        $q = "INSERT INTO recipients
-              SET user_id = {$_user_id},
-                  map_id  = {$_map_id},
-                  name    = '{$_name}',
-                  email   = '{$_email}',
-                  sent    = NULL";
-
-        if($res = mysql_query($q, $db))
-            return mysql_insert_id($db);
-        
-        return null;
-    }
+    require_once 'lib.php';
 
     $db = mysql_connect('localhost', 'safetymaps', 's4f3tym4ps');
     mysql_select_db('safetymaps', $db);
     
-    if(is_array($_POST['sender']) && is_array($_POST['place']) && is_array($_POST['map']) && is_array($_POST['recipients']))
+    if($_POST['sender'] && $_POST['place'] && $_POST['map'] && $_POST['recipients'])
     {
-        mysql_query('BEGIN');
-        $commit_ok = false;
-
-        $user_id = getset_user($db, $_POST['sender']);
-        
-        if($user_id)
-        {
-            $map_args = array(
-                'user_id' => $user_id,
-    
-                'place_name' => $_POST['place']['name'],
-                'place_lat' => $_POST['place']['location'][0],
-                'place_lon' => $_POST['place']['location'][1],
-    
-                'emergency' => $_POST['place']['emergency'],
-                'note_full' => $_POST['place']['full-note'],
-                'note_short' => $_POST['place']['short-note'],
-    
-                'paper' => $_POST['map']['paper'],
-                'format' => $_POST['map']['format'],
-    
-                'bbox_north' => $_POST['map']['bounds'][0],
-                'bbox_south' => $_POST['map']['bounds'][1],
-                'bbox_east' => $_POST['map']['bounds'][2],
-                'bbox_west' => $_POST['map']['bounds'][3],
-    
-                'privacy' => $_POST['map']['privacy']
-            );
-            
-            $map_id = set_map($db, $map_args);
-            
-            if($map_id)
-            {
-                $commit_ok = true;
-            
-                foreach($_POST['recipients'] as $r => $recipient)
-                {
-                    $recipient['user_id'] = $user_id;
-                    $recipient['map_id'] = $map_id;
-                    $recipient_id = set_recipient($db, $recipient);
-                    
-                    if(!$recipient_id)
-                        $commit_ok = false;
-                }
-            }
-        }
-                
-        mysql_query($commit_ok ? 'COMMIT' : 'ROLLBACK');
+        $added_map_id = add_map($db, $_POST);
     }
     
     mysql_close($db);
@@ -204,6 +25,12 @@
     </style>
 </head>
 <body>
+
+<? if($added_map_id) { ?>
+    <p style="color: white; background: red;">
+        Added map #<?=htmlspecialchars($added_map_id)?>
+    </p>
+<? } ?>
 
 <form method="POST" action="map.php">
     <p>
